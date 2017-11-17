b98ae3e3d55a7b4114eaf7dcdc2b49c1e7579cbe
Alex Gorkoff <32149507+alexgorkoff@users.noreply.github.com>
2017-11-17 14:57:30 -0700

    list.h
    -Began definition of FlowList class

--- list.h
+++ list.h
@@ -1,2 +1,22 @@
-#include <fstream>
-using namespace std;
+//list.h
+//ENCM 339 Exercise C
+
+#ifndef LIST_H
+#define LIST_H
+
+class FlowList {
+	
+	public:
+		FlowList(); //default constructor
+		
+	
+	private:
+
+
+
+
+};
+
+#endif
+
+
