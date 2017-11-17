454d8e1823b835389eb12096fcc6484e18edabb1
Matteo-Messana <32150312+Matteo-Messana@users.noreply.github.com>
2017-11-17 15:38:33 -0700

    list.cpp
    -added constructor and default constructor

---
+++ list.cpp
@@ -0,0 +1,20 @@
+// list.cpp
+// ENCM 339 Lab 8 Exercise C 
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#include "list.h"
+
+// default constructor
+FlowList::FlowList()
+{
+	headM = NULL;
+	cursor = NULL;
+}
+
+// constructor
+FlowList::FlowList(const FlowList& source)
+{
+	copy(source);
+}
