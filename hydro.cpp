2975111785f6677ac619b0b09e93f758ca58cd2c
Matteo-Messana <32150312+Matteo-Messana@users.noreply.github.com>
2017-11-18 15:40:42 -0700

    hydro.cpp
    WA

--- hydro.cpp
+++ hydro.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 #include "hydro.h"
 #include "list.h"
+#define SIZE 15;
 
 void displayHeader()
 {
@@ -14,6 +15,22 @@ void displayHeader()
 	cout << "Version: 1.0" << endl;
 	cout << "Lab section: B03" << endl;
 	cout << "Produced by: Alexander Gorkoff and Matteo Messana" << endl;
-	cout << "<<< Press Enter to Continue >>>" << endl;
 	pressEnter();
+}
+
+int readData(FlowList& list)
+{
+	ifstream stream;
+	stream.open("flow.txt");
+	if(stream.fail())
+	{
+		cerr<<"Could not open file."
+		exit(1);
+	}
+	for(int i =0;i<SIZE;i++)
+	{
+		stream<<i+1;
+		list.insert(stream);
+	}
+	
 }
