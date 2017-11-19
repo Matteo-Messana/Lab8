54e39c50bc5290fc1c6a0b23151d121e3c38aae6
Alex Gorkoff <32149507+alexgorkoff@users.noreply.github.com>
2017-11-18 17:52:16 -0700

    Everything
    Makin' good things happen

--- hydro.h
+++ hydro.h
@@ -1,15 +1,17 @@
 //hydro.h
 //ENCM 339 Lab 8 Exercise C
 
+#include "list.h"
+
 #ifndef HYDRO_H
 #define HYDRO_H
 
 void displayHeader();
-// int readData(FlowList& list);
+int readData(FlowList& list);
 int menu();
-// int display(const FlowList& list);
-// void addData(FlowList& list);
-// void removeData(FlowList& list);
+void display(const FlowList& list);
+// int addData(FlowList& list, int size);
+int removeData(FlowList& list, int size);
 // double average(const FlowList& list);
 // double median(const FlowList& list);
 // void saveData(const FlowList& list);
